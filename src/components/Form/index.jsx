19fa4fcfef88962df52835b5d42199281b0cbbe8
Ndiongmo <mauricendiongmo@gmail.com>
2023-02-01 18:04:26 +0100

    Forms state object

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,14 +2,28 @@ import Header from '../Header'
 import React from 'react'
 
 function Formulaire() {
-    const [firstName, setFirstName] = React.useState('')
-    const [lastName, setLastName] = React.useState('')
+    // const [firstName, setFirstName] = React.useState('')
+    // const [lastName, setLastName] = React.useState('')
+
+    const [formData, setFormData] = React.useState({
+        firstName: '',
+        lastName: '',
+    })
+    console.log(formData)
     // console.log(firstName, lastName)
-    function handleFirstName(event) {
-        setFirstName(event.target.value)
-    }
-    function handleLastName(event) {
-        setLastName(event.target.value)
+    // function handleFirstName(event) {
+    //     setFirstName(event.target.value)
+    // }
+    // function handleLastName(event) {
+    //     setLastName(event.target.value)
+    // }
+    function handleChange(event) {
+        setFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value,
+            }
+        })
     }
     return (
         <div className="App">
@@ -18,13 +32,17 @@ function Formulaire() {
                 <input
                     type="text"
                     placeholder="First Name"
-                    onChange={handleFirstName}
+                    // onChange={handleFirstName}
+                    onChange={handleChange}
+                    name="firstName"
                     style={{ marginRight: '15px' }}
                 />
                 <input
                     type="text"
                     placeholder="Last Name"
-                    onChange={handleLastName}
+                    // onChange={handleLastName}
+                    onChange={handleChange}
+                    name="lastName"
                 />
             </form>
         </div>
